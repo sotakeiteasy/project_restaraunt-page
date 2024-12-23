8af852ccb2e608f1d7bf82ca442f97179455a717
sotakeiteasy <murasov96@gmail.com>
2024-12-23 20:15:06 +0300

    add reserve to main

---
+++ webpack.common.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       title: 'Production',
+//     }),
+//   ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  devServer: {
+      watchFiles: ["./src/template.html"],
+    },
+  plugins: [
+        new HtmlWebpackPlugin({
+        template: "./src/template.html",
+        }),
+    ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    //   {
+    //     test: /\.html$/i,
+    //     loader: "html-loader",
+    //   },      
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  
+};
